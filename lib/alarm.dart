eed07b002dbe873e5a477f2c6eee2544e45b1bf8
coderkhem <khemagarwal1@gmail.com>
2020-07-01 01:25:11 +0530

    Pop up  Set Alarm

--- lib/alarm.dart
+++ lib/alarm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class alarm extends StatefulWidget {
   @override
@@ -8,9 +9,51 @@ class alarm extends StatefulWidget {
 class _alarmState extends State<alarm> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text("alarm"),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.fromLTRB(100.0,200.0,150.0,10.0),
+        child: FloatingActionButton(
+
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    content: Stack(
+                      overflow: Overflow.visible,
+                      children: <Widget>[
+                        Positioned(
+                          right: -40.0,
+                          top: -40.0,
+                          child: InkResponse(
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: CircleAvatar(
+                              child: Icon(Icons.close),
+                              backgroundColor: Colors.red,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child:
+                          Column(
+                            children: <Widget>[
+                              
+                              Text("Set Time"),
+                            ],
+                          ),
+                        ),
+
+
+                      ],
+                    ),
+                  );
+                });
+          },
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
