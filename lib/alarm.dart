2efc52efa8c97f7ad2ab8e339707f9e1616896d1
coderkhem <khemagarwal1@gmail.com>
2020-07-05 03:56:25 +0530

    Pop up  Set Alarm changes

--- lib/alarm.dart
+++ lib/alarm.dart
@@ -1,5 +1,10 @@
+//import 'dart:html';
+
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class alarm extends StatefulWidget {
   @override
@@ -7,15 +12,24 @@ class alarm extends StatefulWidget {
 }
 
 class _alarmState extends State<alarm> {
+
+
+
   @override
   Widget build(BuildContext context) {
+    double fromL=(MediaQuery.of(context).size.width/2)-50;
+    double fromT=10;
+    double fromR=(MediaQuery.of(context).size.width/2)-50;
+    double fromB=10;
+
     return Scaffold(
       backgroundColor: Colors.black,
       floatingActionButton: Padding(
-        padding: const EdgeInsets.fromLTRB(100.0,200.0,150.0,10.0),
+        padding: EdgeInsets.fromLTRB(fromL,fromT,fromR,fromB),
         child: FloatingActionButton(
 
           onPressed: () {
+
             showDialog(
                 context: context,
                 builder: (BuildContext context) {
@@ -36,21 +50,36 @@ class _alarmState extends State<alarm> {
                             ),
                           ),
                         ),
-                        Container(
-                          child:
-                          Column(
-                            children: <Widget>[
-                              
-                              Text("Set Time"),
-                            ],
-                          ),
+                        Wrap(
+                          children: <Widget>[
+                            Container(
+
+                              child:
+                              Column(
+                                children: <Widget>[
+
+                                  Text("Set Time",
+                                    style: TextStyle(
+                                      color: Colors.blue[900].withOpacity(1.0),
+                                      fontSize: 30.0,
+                                      fontWeight: FontWeight.bold,
+
+                                    ),
+                                  ),
+
+                                ],
+                              ),
+                            ),
+                          ],
                         ),
+
 
 
                       ],
                     ),
                   );
                 });
+
           },
           child: const Icon(Icons.add),
         ),
